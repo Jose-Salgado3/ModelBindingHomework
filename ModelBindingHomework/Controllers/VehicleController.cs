c127e8a427a9c543bc8c1fe4ab0af1747f918fa3
Jose Salgado <Josalgado03@gmail.com>
2019-07-27 10:05:42 -0700

    Add project files.

---
+++ ModelBindingHomework/Controllers/VehicleController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingHomework.Models;
+
+namespace ModelBindingHomework.Controllers
+{
+    public class VehicleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(IFormCollection data)
+        {
+
+            Vehicle v = new Vehicle()
+            {
+                FullName = data["full-name"],
+                Make = data["make"],
+                Model = data["model"],
+                Year = Convert.ToInt32(data["year"]),
+                Miles = Convert.ToInt32(data["miles"])
+            };
+
+            //ADD TO DATABASE HERE!!!!
+            return View();
+        }
+
+
+        //FOR Homework here is the model binding View!
+        [HttpGet]
+        public IActionResult ApplyMB()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ApplyMB(Vehicle v)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add to database
+                // Alert successs
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ ModelBindingHomework/Models/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingHomework.Models
+{
+    /// <summary>
+    /// Represents the vehicle that is to be rented
+    /// Full Name is the person that is renting it
+    /// </summary>
+    public class Vehicle
+    {
+        /// <summary>
+        /// The legal First and Last name of the person wishing to rent a vehicle
+        /// </summary>
+
+        [Required]
+        public string FullName { get; set; }
+        
+        /// <summary>
+        /// The name of the company that manufactured the vehicle.
+        /// </summary>
+        [Required]
+        public string Make { get; set; }
+
+        /// <summary>
+        /// The model of the vehicle that is to be rented
+        /// </summary>
+        [Required]
+        public string Model { get; set; }
+
+
+        /// <summary>
+        /// The year that the vehicle was manufactured
+        /// ex. 1990
+        /// </summary>
+        [Required]
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Represents the miles that are on the vehicle at the current time of applying.
+        /// </summary>
+        [Required]
+        public int Miles { get; set; }
+    }
+}
